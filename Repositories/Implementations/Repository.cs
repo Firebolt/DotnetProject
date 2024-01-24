e77224ec9fa1745deab2787ffca29ac75ca22d65
Pallaeva <150933852+Pallaeva@users.noreply.github.com>
2024-01-25 02:23:21 +0300

    Generic Repository Pattern
    A generic repository provides a standardized way to perform basic CRUD (Create, Read, Update, Delete) operations on various entities within our application. This can help improve the maintainability, testability, and overall structure of our code.

---
+++ Repositories/Implementations/Repository.cs
@@ -0,0 +1,55 @@
+﻿using FinalProject.Models;
+using FinalProject.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FinalProject.Repositories.Implementations
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await Save();
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+                await Save();
+            }
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
